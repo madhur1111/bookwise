554772585015f58d1dea5f36c4f40ee04a166db1
madhur1111 <madhursalunke07@gmail.com>
2025-09-14 17:44:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
 // --- MongoDB Connection ---
-const mongoose = require("mongoose");
+
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
